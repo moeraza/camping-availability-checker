1fafe89432154f1240ec4ed2c7342a90eb8a45d5
Mohammad Raza <mohammedraza100@gmail.com>
2023-06-30 15:00:01 -0600

    adding text message

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ from __future__ import annotations
 
 import asyncio
 
+from notifciation import TwilioManager
 from scraper import DataCollector
 
 
 async def main():
     dc = DataCollector(start_date='2023-08-22', end_date='2023-08-23')
-
     await dc.initialize()
 
     data = await dc.extract_data()
+
+    if len(data) > 0:
+        tm = TwilioManager()
+        tm.send_message(message='Camp is ready!', to='+16478337641')
 
     print(data)
 
