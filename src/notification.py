ceeaf4307bf72c7573cd8131ee90e636c2b89224
Mohammad Raza <mohammedraza100@gmail.com>
2023-06-30 14:52:06 -0600

    adding message sender

---
+++ src/notification.py
@@ -0,0 +1,39 @@
+"""
+Example usage of class:
+
+tm = TwilioManager()
+tm.send_message(message='Camp is ready!', to='+16478337641')
+
+"""
+from __future__ import annotations
+
+import os
+
+from dotenv import load_dotenv
+from twilio.rest import Client
+
+
+class TwilioManager:
+    def __init__(self):
+        load_dotenv()
+        self.account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+        self.auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+        self.from_number = os.getenv('TWILIO_NUMBER')
+        self.client = Client(self.account_sid, self.auth_token)
+
+    def send_message(self, message: str, to: str):
+        """Send a text message using twilio
+
+        Parameters
+        ----------
+        message : str
+            the message that you would like to send
+        to : str
+            the phone number you would like to send it to
+        """
+        response = self.client.messages.create(
+            from_=self.from_number,
+            body=message,
+            to=to,
+        )
+        print(f'Message sent! Message SID: {response.sid}')
